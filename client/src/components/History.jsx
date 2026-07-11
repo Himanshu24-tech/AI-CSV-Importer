6c44dc6de886c8e8c9f2b0fc9d18b420b7bcc04d
Himanshu Pardhi <himanshupardhi792@gmail.com>
2026-07-11 17:30:29 +0530

    Update History.jsx

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -1,22 +1,22 @@
-export default function History() {
+// export default function History() {
 
-    const history = JSON.parse(
-        localStorage.getItem("importHistory") || "[]"
-    );
+//     const history = JSON.parse(
+//         localStorage.getItem("importHistory") || "[]"
+//     );
 
-    if (!history.length) return null;
+//     if (!history.length) return null;
 
-    return (
-        <div className="mt-10 bg-white shadow rounded-xl p-5">
+//     // return (
+//         <div className="mt-10 bg-white shadow rounded-xl p-5">
 
-            <h2 className="text-xl font-bold mb-4">
-                Import History
-            </h2>
+//             <h2 className="text-xl font-bold mb-4">
+//                 Import History
+//             </h2>
 
-            <pre className="text-sm overflow-auto">
-                {JSON.stringify(history, null, 2)}
-            </pre>
+//             <pre className="text-sm overflow-auto">
+//                 {JSON.stringify(history, null, 2)}
+//             </pre>
 
-        </div>
-    );
-}
+//         </div>
+//     );
+// }
